34c9ed935f49145d482dd3c49d26bf48c4f20ff3
Denys Telezhkin <strangervir@gmail.com>
2014-02-10 15:31:24 +0200

    rename targets, add initial example controller

---
+++ DTPickerPresenterTests/DTPickerPresenterExample/PickerController.h
@@ -0,0 +1,13 @@
+//
+//  PickerController.h
+//  DTPickerPresenter
+//
+//  Created by Denys Telezhkin on 10.02.14.
+//  Copyright (c) 2014 MLSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerController : UIViewController
+
+@end
---
+++ DTPickerPresenterTests/DTPickerPresenterExample/PickerController.m
@@ -0,0 +1,18 @@
+//
+//  PickerController.m
+//  DTPickerPresenter
+//
+//  Created by Denys Telezhkin on 10.02.14.
+//  Copyright (c) 2014 MLSDev. All rights reserved.
+//
+
+#import "PickerController.h"
+
+@interface PickerController ()
+
+@end
+
+@implementation PickerController
+
+
+@end
