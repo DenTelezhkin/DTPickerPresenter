2808e684475c0764c7075b4574f05b905114e36f
JM <jose.navarro@liferay.com>
2014-08-27 13:04:26 +0200

    Changed sample to show how to create basic custom presenters

---
+++ DTPickerPresenterTests/DTPickerPresenterExample/CustomViewController.m
@@ -0,0 +1,41 @@
+//
+//  CustomViewController.m
+//  DTPickerPresenter
+//
+//  Created by jmWork on 27/08/14.
+//  Copyright (c) 2014 MLSDev. All rights reserved.
+//
+
+#import "CustomViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonTouch:(id)sender {
+	NSLog(@"button touched!");
+}
+@end
